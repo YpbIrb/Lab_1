ed95f382f16238e3fdb50798b8e0868d650239da
Smal_Ivan <32317297+YpbIrb@users.noreply.github.com>
2017-10-11 10:28:01 +0700

    Add files via upload

---
+++ lab1_1.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+
+void set_stop_symb(char model, int length, int arr_of_stop){
+	int i;
+	int stop;
+	char dop_char;
+	char symb;
+	dop_char = *(model+length);
+	*(model + length) = 0;
+
+	for (i = 0; i < length; i++){
+		symb = *(model+i);
+		stop = strrchr(model, symb) - model;
+		*(arr_of_stop + i) = stop;
+
+	}
+
+	*(model + length) = dop_char;
+
+
+}
+
+
+int main(){
+	char model[16];
+	gets(model);
+	
+
+
+	return 0;
+}
