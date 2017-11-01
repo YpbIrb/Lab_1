eb3ec453782f22976893e1e1c4c06c85bb852e57
Smal_Ivan <32317297+YpbIrb@users.noreply.github.com>
2017-11-01 10:13:52 +0700

    Update lab1_1.c

--- lab1_1.c
+++ lab1_1.c
@@ -1,33 +1,32 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
-
-void set_stop_symb(char model, int length, int arr_of_stop){
+//установка стоп символов
+void set_stop_symb(char* model, int length, int* stop_symb){
 	int i;
 	int stop;
 	char dop_char;
 	char symb;
-	dop_char = *(model+length);
-	*(model + length) = 0;
-
-	for (i = 0; i < length; i++){
-		symb = *(model+i);
+	dop_char = *(model + length - 1);
+	*(model + length - 1) = 0;
+	for (i = 0; i < length - 1; i++){
+		symb = *(model + i);
 		stop = strrchr(model, symb) - model;
-		*(arr_of_stop + i) = stop;
-
+		*(stop_symb +i) = stop;
+		
 	}
-
-	*(model + length) = dop_char;
-
-
+	*(model + length - 1) = dop_char;
 }
 
 
 int main(){
-	char model[16];
+
+	char model[16] = {0};
 	gets(model);
+	int len_model = strlen(model);
+	int stop_symb[16] = {0};
+	set_stop_symb(model, len_model, stop_symb);
 	
-
-
+	
 	return 0;
 }
